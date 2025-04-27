c51b2504eded79bc3366530d34abed482dd1877d
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-04-27 15:34:57 +0100

    SCRUM-27 / Added environment variable for apiSecret

--- src/main/java/lanz/global/customerservice/CustomerServiceApplication.java
+++ src/main/java/lanz/global/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package lanz.global.customerservice;
 
+import lanz.global.customerservice.api.config.ServiceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableConfigurationProperties(ServiceConfig.class)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
