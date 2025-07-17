d2a7fd67fe18e396723f27b03d8cce63e86327c0
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-07-17 23:37:18 +0100

    Removed logging

--- src/main/java/lanz/global/customerservice/service/FinanceService.java
+++ src/main/java/lanz/global/customerservice/service/FinanceService.java
@@ -4,14 +4,12 @@ import lanz.global.customerservice.external.api.finance.FinanceClient;
 import lanz.global.customerservice.external.api.finance.request.GetContractParams;
 import lanz.global.customerservice.external.api.finance.response.ContractResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
-@Log4j2
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class FinanceService {
@@ -21,10 +19,7 @@ public class FinanceService {
     public boolean customerContainsLinkedContracts(UUID customerId) {
         GetContractParams params = new GetContractParams();
         params.setCustomerId(customerId);
-        log.debug("Searching for contracts linked to customer with ID: {}", customerId);
         Page<ContractResponse> response = financeClient.findAllByFilter(params);
-
-        log.debug("Contracts {}", response.getTotalElements());
         return response.getTotalElements() > 0;
     }
 }
