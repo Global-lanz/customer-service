f5607adfeb7972e9ae506de6fd4945e74b5f9d19
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-08-04 00:01:58 +0100

    SCRUM-42 / Added token validation

--- src/main/java/lanz/global/customerservice/security/SecurityConfig.java
+++ src/main/java/lanz/global/customerservice/security/SecurityConfig.java
@@ -20,11 +20,13 @@ import javax.crypto.spec.SecretKeySpec;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private static final String ROLE_SERVICE = "ROLE_SERVICE";
     private final ServiceConfig config;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, "/v3/api-docs").permitAll()
+                        .requestMatchers("/finance/**").hasAnyRole(ROLE_SERVICE)
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
