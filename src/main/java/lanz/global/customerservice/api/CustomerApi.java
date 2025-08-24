87ef3a1ab940d16417294cc2a4f46297c1ee6231
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2025-08-24 16:54:30 +0100

    SCRUM-43 / Added documentation

--- src/main/java/lanz/global/customerservice/api/CustomerApi.java
+++ src/main/java/lanz/global/customerservice/api/CustomerApi.java
@@ -1,5 +1,8 @@
 package lanz.global.customerservice.api;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import jakarta.annotation.security.RolesAllowed;
 import lanz.global.customerservice.api.config.Rules;
 import lanz.global.customerservice.api.request.customer.CustomerRequest;
@@ -35,6 +38,8 @@ public class CustomerApi {
 
     @PostMapping
     @RolesAllowed(Rules.CREATE_CUSTOMER)
+    @ApiOperation(value = "Create customers", notes = "The endpoint creates a customers")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "List of customers"), @ApiResponse(code = 400, message = "Bad request")})
     public ResponseEntity<CustomerResponse> createCustomer(@RequestBody CustomerRequest customerRequest) {
         Customer createdCustomer = customerService.createCustomer(customerRequest);
 
@@ -43,6 +48,8 @@ public class CustomerApi {
 
     @GetMapping
     @RolesAllowed(Rules.LIST_CUSTOMERS)
+    @ApiOperation(value = "List customers", notes = "The endpoint retrieves a list of customers")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "List of customers")})
     public ResponseEntity<FindCustomersResponse> getCustomersFromCompany() {
         List<Customer> customers = customerService.getCustomersFromCompany();
 
@@ -54,6 +61,8 @@ public class CustomerApi {
 
     @GetMapping("/search")
     @RolesAllowed(Rules.LIST_CUSTOMERS)
+    @ApiOperation(value = "Filter customers", notes = "The endpoint retrieves a list of customers based on filter criteria")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "List of customers")})
     public ResponseEntity<Page<CustomerResponse>> filterCustomers(@ModelAttribute GetCustomerParams params) {
         Page<Customer> page = customerService.filterCustomers(params);
 
@@ -62,6 +71,8 @@ public class CustomerApi {
 
     @GetMapping("/{customerId}")
     @RolesAllowed({Rules.GET_CUSTOMER, Rules.M2M})
+    @ApiOperation(value = "Get customer by ID", notes = "The endpoint retrieves a customers based on its ID")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Customer data"), @ApiResponse(code = 404, message = "Not found")})
     public ResponseEntity<CustomerResponse> findCustomerById(@PathVariable UUID customerId) {
         Customer customer = customerService.findCustomerById(customerId);
 
@@ -70,6 +81,8 @@ public class CustomerApi {
 
     @PutMapping("/{customerId}")
     @RolesAllowed(Rules.UPDATE_CUSTOMER)
+    @ApiOperation(value = "Updates a customer", notes = "The endpoint updates an existing customer")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "List of customers"), @ApiResponse(code = 404, message = "Customer not found"), @ApiResponse(code = 400, message = "Bad request")})
     public ResponseEntity<CustomerResponse> updateCustomer(@PathVariable UUID customerId, @RequestBody CustomerRequest customerRequest) {
         Customer customer = customerService.updateCustomer(customerId, customerRequest);
         return ResponseEntity.ok(serviceConverter.convert(customer, CustomerResponse.class));
@@ -77,6 +90,8 @@ public class CustomerApi {
 
     @DeleteMapping("/{customerId}")
     @RolesAllowed(Rules.DELETE_CUSTOMER)
+    @ApiOperation(value = "Delete customer", notes = "The endpoint deletes a customer based on its ID")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Customer deleted"), @ApiResponse(code = 404, message = "Customer not found")})
     public ResponseEntity<Void> deleteCustomer(@PathVariable UUID customerId) {
         customerService.deleteCustomer(customerId);
         return ResponseEntity.noContent().build();
